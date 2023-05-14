2fad0fc50c2717756f29138f4c21d5dc56039e75
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2023-05-14 11:02:53 -0300

    clearBoard inside gameBoard module

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ const gameBoard = (() => {
   // 3x3 matrix filled with -1
   const board = new Array(3).fill().map(() => new Array(3).fill(-1));
 
-  return {
-    board,
-  };
-})();
-
-const gameLogic = (() => {
-  const clearBoard = (board) => {
+  const clearBoard = () => {
     // fill the board with -1
     board.forEach((row, rowIndex) => {
       const resetRow = row.map(() => -1);
