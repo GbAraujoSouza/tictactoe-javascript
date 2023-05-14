6b763a5b10d1780a6bf4a5f262926c382208c4f9
GbAraujoSouza <gabriel.souzaaraujo109@gmail.com>
2023-05-14 18:13:49 -0300

    make 2 players and bound event to cell click

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-/* eslint-disable quotes */
-const makePlayer = (name, marker) => {
-  const choseCell = (row, column, board) => {
-    // check if cell's already marked
-    if (board[row][column] !== -1) {
-      return board[row][column];
+const makePlayer = (marker) => {
+  const choseValidCell = (row, column, board) => {
+    if (row === undefined || column === undefined) {
+      return false;
     }
+    // check if cell's already marked
+    if (board[row][column] === -1) {
+      return true;
+    }
+    return false;
   };
 
-  return { name, marker, choseCell };
+  return { marker, choseValidCell };
 };
 
 const gameBoard = (() => {
+  const rowLength = 3;
+  const columnLength = 3;
   // 3x3 matrix filled with -1
   const board = new Array(3).fill().map(() => new Array(3).fill(-1));
 
@@ -18,21 +23,83 @@ const gameBoard = (() => {
     // fill the board with -1
     board.forEach((row, rowIndex) => {
       const resetRow = row.map(() => -1);
-      board[rowIndex] = resetRow; // eslint-disable-line no-param-reassign
+      board[rowIndex] = resetRow;
     });
   };
 
+  const getBoardMatrix = () => board;
+
+  const updateBoard = () => {
+    console.table(board);
+  };
+
   return {
+    rowLength,
+    columnLength,
+    getBoardMatrix,
     clearBoard,
+    updateBoard,
   };
 })();
 
-const gameLogic = ((board) => {
-  const resetGame = () => {
-    board.clearBoard();
+const gameLogic = ((boardObj) => {
+  let currentPlayer = 1;
+  const players = [makePlayer('O'), makePlayer('X')];
+
+  const startGame = () => {
+    currentPlayer = 0;
+    boardObj.clearBoard();
   };
 
+  const resetGame = () => {
+    boardObj.clearBoard();
+  };
+
+  const markCell = (row, column, marker, board) => {
+    board[row][column] = marker; // eslint-disable-line no-param-reassign
+  };
+
+  function handleClick(e) {
+    const element = e.target;
+    const selectedRow = Number.parseInt(
+      Math.floor(element.dataset.cell / boardObj.rowLength),
+      10,
+    );
+    const selectedColumn = Number.parseInt(
+      element.dataset.cell % boardObj.rowLength,
+      10,
+    );
+
+    if (
+      players[currentPlayer].choseValidCell(
+        selectedRow,
+        selectedColumn,
+        boardObj.getBoardMatrix(),
+      )
+    ) {
+      markCell(
+        selectedRow,
+        selectedColumn,
+        players[currentPlayer].marker,
+        boardObj.getBoardMatrix(),
+      );
+      boardObj.updateBoard();
+      currentPlayer = 1 - currentPlayer;
+    }
+    // update display
+  }
+
   return {
+    startGame,
     resetGame,
+    handleClick,
   };
 })(gameBoard);
+
+const domHandler = (() => {
+  // cache DOM elements
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach((cell) => {
+    cell.addEventListener('click', gameLogic.handleClick);
+  });
+})();
